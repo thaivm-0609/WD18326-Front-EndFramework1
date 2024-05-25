fa48b1940bd170c8f70dc8481a146e5fdf1f6a4e
thaivm-0609 <vuongthai95@gmail.com>
2024-05-25 17:23:19 +0700

    feat: create

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -1,12 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './create.component.html',
   styleUrl: './create.component.css'
 })
 export class CreateComponent {
+  apiUrl = 'http://localhost:3000/projects';
 
+  constructor(
+    private http:HttpClient,
+    private router:Router
+  ) {}
+  onCreate(data: any): void {
+    this.http.post(
+      this.apiUrl,
+      data
+    ).subscribe(res => {
+      alert('Thêm mới thành công');
+      this.router.navigate(['/list']);
+    })
+  }
 }
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -12,15 +12,24 @@ import { RouterLink } from '@angular/router';
 
 export class ListComponent {
   listProjects: any;
+  apiUrl = 'http://localhost:3000/projects'; //khai báo apiUrl
+
   constructor(private thaivm2:HttpClient) {}
 
   ngOnInit(): void {
-    let apiUrl = 'http://localhost:3000/projects'; //khai báo apiUrl
-
     //cú pháp gửi http request
     //this.HttpClient.method(apiUrl).subscribe( response => {})
-    this.thaivm2.get(apiUrl).subscribe(response => {
+    this.thaivm2.get(this.apiUrl).subscribe(response => {
       this.listProjects = response;
     })
+  }
+
+  onDelete(id: any): void {
+    if(confirm('Bạn có chắc không?')) { //popup confirm khi xóa
+      this.thaivm2.delete(this.apiUrl + '/' + id).subscribe(res => {
+        alert('Xóa thành công');
+        this.ngOnInit(); //cập nhật lại danh sách project mà ko cần reload lại trang
+      })
+    } 
   }
 }
