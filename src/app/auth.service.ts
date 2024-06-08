5486c572ce77792b2c4f58789c55efee7aac7027
thaivm-0609 <vuongthai95@gmail.com>
2024-06-08 17:01:38 +0700

    feat: check user login

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { jwtDecode } from 'jwt-decode';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -17,5 +18,30 @@ export class AuthService {
   //hàm đăng nhập
   login = (data: any): Observable<any> => {
     return this.http.post(this.apiUrl+'/login', data);
+  }
+
+  checkAuthUser=():boolean => {
+    let valid = false;
+    try {
+      const userinfo = localStorage.getItem('user');
+      if (userinfo !== null) {
+        const userObj = JSON.parse(userinfo);
+        const token = jwtDecode(userObj?.token); //decode token để lấy thời gian hết hạn
+        
+        //so sánh thời gian token hết hạn với thời gian hiện tại Date.now()/1000
+        //nếu exp lớn hơn->token vẫn còn thời gian sử dụng
+        //nếu exp nhỏ hơn->token hết hạn
+        if (token.exp as any > Date.now()/1000) {
+          valid = true;
+        } else {
+          valid = false;
+        }
+      }
+    } catch (error) {
+      alert(error);
+      return false;
+    }
+
+    return valid;
   }
 }
--- src/app/can-activate-guard.guard.ts
+++ src/app/can-activate-guard.guard.ts
@@ -1,10 +1,14 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { AuthService } from './auth.service';
 
 export const canActivateGuardGuard: CanActivateFn = (route, state) => {
-  let test = '123';
-  if (test == '123') { 
+  const authService = inject(AuthService);
+  const router = new Router();
+  if (authService.checkAuthUser()) { 
     return true; //đk thỏa mãn thì được truy cập
-  }
-  
-  return false; //đk false thì không được truy cập
+  } else {
+    router.navigate(['login']);
+    return false; //đk false thì không được truy cập
+  }  
 };
